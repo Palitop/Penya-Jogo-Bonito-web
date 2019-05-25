63e59555c4fdc27fce90b51492ed5fcc3e1b1eb1
Pau Pérez Rebassa <pauperez14@gmail.com>
2019-05-25 22:59:03 +0200

    Small brushstrokes of materialize

--- js/logic.js
+++ js/logic.js
@@ -3,4 +3,9 @@ window.onload = function(){
     document.getElementById("boton").onclick = function() {
         console.log(document.getElementById("texto").value);
     };
+
+    document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.parallax');
+        var instances = M.Parallax.init(elems);
+    });
 }
